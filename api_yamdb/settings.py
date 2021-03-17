68ba7c16322af1c0483382118a8e5b0757d54f68
bogdanov <bogdanov2000@gmail.com>
2021-03-17 04:43:05 +0300

    Commit x9. Update hosts

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(', ')
+#ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(', ')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
 
 # Application definition
 INSTALLED_APPS = [
