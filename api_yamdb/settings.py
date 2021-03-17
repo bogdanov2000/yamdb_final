89fececbc256057d2ba9d0445091953e7f4eaa56
bogdanov <bogdanov2000@gmail.com>
2021-03-17 20:40:02 +0300

    Commit x26. Back to no Base_dir

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -2,8 +2,10 @@ import os
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.environ.get('BASE_DIR')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# BASE_DIR = os.environ.get('BASE_DIR')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
